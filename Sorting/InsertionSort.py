51bfc180635d5023c7c67118051e4a13c9212c40
minnukota381 <minnukota381@gmail.com>
2024-11-03 12:00:00 +0530

    InsertionSort.c

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,12 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+arr = list(map(int, input("Enter the array elements separated by space: ").split()))
+insertion_sort(arr)
+print("Sorted array:", arr)
