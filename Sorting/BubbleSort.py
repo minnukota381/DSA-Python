7eb36262b829e967afe0fcabdd186f7494b8e6c6
minnukota381 <minnukota381@gmail.com>
2025-01-01 13:48:36 +0530

    Add alternative bubble sort implementation without using swapped variable

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -12,3 +12,20 @@ def bubble_sort(arr):
 arr = list(map(int, input("Enter the array elements separated by space: ").split()))
 bubble_sort(arr)
 print("Sorted array:", arr)
+
+## Without using Swapped variable
+        
+def bubbleSort(arr1):
+    n=len(arr1)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr1[j]>arr1[j+1]:
+                arr1[j], arr1[j+1]=arr1[j+1], arr1[j]
+                
+    print("Sorted array:", arr1)
+    
+arr1=[64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr1)
+                
+
+
